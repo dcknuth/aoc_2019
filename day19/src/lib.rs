92f54b241bf82f69a765def445fd92d7139652b4
dcknuth <36804076+dcknuth@users.noreply.github.com>
2026-02-11 13:05:28 -0800

    d19 Rust v2

--- day19/src/lib.rs
+++ day19/src/lib.rs
@@ -1,10 +1,10 @@
 use imac::Imac;
-use std::collections::{VecDeque, HashMap};
+use std::collections::VecDeque;
 
 pub fn part1(s: &String) -> i64 {
     let p: Vec<i64> = s.split(',')
     .map(|i| i.trim().parse().unwrap()).collect();
-    let mut vm = Imac::new(&p, 0, Some(VecDeque::new()));
+    let vm = Imac::new(&p, 0, Some(VecDeque::new()));
     let mut total = 0;
     for y in 0..50 {
         for x in 0..50 {
@@ -27,18 +27,18 @@ pub fn part1(s: &String) -> i64 {
 pub fn part2(s: &String) -> i64 {
     let p: Vec<i64> = s.split(',')
     .map(|i| i.trim().parse().unwrap()).collect();
-    let mut vm = Imac::new(&p, 0, Some(VecDeque::new()));
+    let vm = Imac::new(&p, 0, Some(VecDeque::new()));
 
     // will have the starting and ending x position for row y
-    let mut beam_width: HashMap<i64, [i64; 2]> = HashMap::new();
+    let mut beam_width: Vec<[i64; 2]> = Vec::new();
     let start_y = 1500;
     let start_x = 1650;
     let mut y = start_y;
     loop {
         let mut beam_start = 0;
         let mut beam_end = 0;
-        // will do this later: let mut line_done = false;
-        for x in start_x..(start_x+800) {
+        let mut line_done = false;
+        for x in (start_x+(y-start_y))..(start_x+800) {
             let mut cur_vm = vm.clone();
             cur_vm.load_in(&mut VecDeque::from(vec![x, y]));
             cur_vm.run();
@@ -49,15 +49,22 @@ pub fn part2(s: &String) -> i64 {
                     } else {
                         beam_end = x;
                     }
+                } else if beam_end != 0 {
+                    line_done = true;
                 }
             } else {
                 eprintln!("Error: we should always get an output")
             }
+            if line_done {
+                break;
+            }
         }
-        beam_width.insert(y, [beam_start, beam_end]);
+        beam_width.push([beam_start, beam_end]);
         if y >= start_y + 99 {
-            if beam_width[&(y - 99)][1] - beam_width[&y][0] >= 99 {
-                return (beam_width[&(y - 99)][1] - 99) * 10000 + (y - 99)
+            if beam_width[((y - start_y) - 99) as usize][1] -
+            beam_width[(y - start_y) as usize][0] >= 99 {
+                let ans = (beam_width[((y - start_y) - 99) as usize][1] - 99) * 10000 + (y - 99);
+                return ans
             }
         }
         y += 1;
